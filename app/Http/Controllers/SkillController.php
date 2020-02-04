5124c767927ae9951e1b5cf836057101827add3c
Ricardo Salas <ricardevelopment@gmail.com>
2020-02-04 12:24:24 +0100

    Endpoint para traer un array de objetos de la tabla skills

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Offer;
+use App\Models\Skill;
+use App\Models\Employee;
+
+
+class SkillController extends Controller
+{
+    public function findSkills(){
+
+            return  response()->json(Skill::get());
+        
+    }
+
+    public function applySkill($skillId, $uid){
+	
+		
+        try {
+
+                // busco la skill cotejando con su id 
+
+                $skill = Skill::find($skillId);
+
+                // Busco el employee cotejando la uid recibida
+
+                $skillTaker = Employee::where("uid", "=", $uid) -> first();
+
+                // si no existe el employee busca la uid en offer
+
+               if ( $skillTaker -> isEmpty() ) {
+
+                    $skillTaker = Offer::where("uid", "=", $uid) -> first();
+
+                    // Me registro en la oferta añadiendo los id de oferta e id 
+                    // de skill en la tabla intermedia
+
+                    $skill->ownerOffer()->attach($skillTaker->id);
+                    
+                    
+		        }else{
+
+                    // Me registro en la oferta añadiendo los id de employee e id 
+                    // de oferta en la tabla intermedia
+
+                    $skill->ownerEmployee()->attach($skillTaker->id);
+                }
+             
+                
+				
+				
+				return response() -> json([
+                    "success" => "e",
+                ]);
+				
+			} catch(\Illuminate\Database\QueryException $e){
+                
+                $errorCode = $e->errorInfo[1];
+                
+                
+                if ($errorCode == 1062) {
+                    return response()->json([
+                        'error' => "no se encontro ningun resultado",
+                        'errorCode' => "offer_find_1"
+                    ], 404);            
+                };
+                
+                
+                return $e->errorInfo;
+			
+		};
+	}
+	
+	public function getAppliedSkills($uid){
+	
+		
+        try {
+				
+               
+                 // Busco el employee cotejando la uid recibida
+
+                 $skillTaker = Employee::where("uid", "=", $uid) -> first();
+
+                 // si no existe el employee busca la uid en offer
+ 
+                if ( $skillTaker -> isEmpty() ) {
+ 
+                     $skillTaker = Offer::where("uid", "=", $uid) -> first();
+                }
+
+                // Busco todas las skills a las que se ha inscrito el employee u offer
+
+                $result = $skillTaker->skills;
+               
+                // Recorro el array de ofertas suscritas
+
+                foreach($result as $offer){
+
+                    $company_id = $offer->company_id;
+
+                    // Busco la empresa cotejando la id de la oferta
+
+                    $company = Company::where("id", "=", $company_id) -> get();   
+
+                    // Incorporo el nombre de la empresa en el objeto de la oferta
+
+                    $offer['companyName'] = $company[0]->name;
+
+                }
+				
+				return response() -> json($result);
+				
+			} catch(\Illuminate\Database\QueryException $e){
+                
+                $errorCode = $e->errorInfo[1];
+                
+                
+                if ($errorCode == 1062) {
+                    return response()->json([
+                        'error' => "no se encontro ningun resultado",
+                        'errorCode' => "company_find_1"
+                    ], 404);            
+                };
+                
+                
+                return $e->errorInfo;
+			
+		};
+	}
+}
